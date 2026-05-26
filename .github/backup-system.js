1ca6014e40ce176eef85c58708141e55b35a221c
ledo386 <davidoskipro08.2.0@gmail.com>
2026-05-26 15:49:15 +0200

    Create backup-system.js

---
+++ .github/backup-system.js
@@ -0,0 +1,125 @@
+// ==========================
+// BACKUP MANUAL
+// ==========================
+
+function runBackup(){
+
+  const backups =
+  JSON.parse(
+    localStorage.getItem("backups")
+  ) || [];
+
+  const newBackup = {
+
+    date:
+    new Date().toLocaleString(),
+
+    type:"Manual",
+
+    status:"Completado",
+
+    size:
+    Math.floor(Math.random()*5000)
+    + " MB"
+
+  };
+
+  backups.push(newBackup);
+
+  localStorage.setItem(
+    "backups",
+    JSON.stringify(backups)
+  );
+
+  addAlert(
+    "Copia manual completada"
+  );
+
+  loadHistory();
+
+  alert(
+    "Copia de seguridad completada."
+  );
+
+}
+
+// ==========================
+// BACKUP AUTOMÁTICO
+// ==========================
+
+function automaticBackup(){
+
+  const backups =
+  JSON.parse(
+    localStorage.getItem("backups")
+  ) || [];
+
+  backups.push({
+
+    date:
+    new Date().toLocaleString(),
+
+    type:"Automático",
+
+    status:"Completado",
+
+    size:
+    Math.floor(Math.random()*5000)
+    + " MB"
+
+  });
+
+  localStorage.setItem(
+    "backups",
+    JSON.stringify(backups)
+  );
+
+  addAlert(
+    "Backup automático ejecutado"
+  );
+
+}
+
+// Cada 60 segundos
+
+setInterval(() => {
+
+  automaticBackup();
+
+}, 60000);
+
+// ==========================
+// RESTAURAR BACKUP
+// ==========================
+
+function restoreBackup(){
+
+  const restores =
+  JSON.parse(
+    localStorage.getItem("restores")
+  ) || [];
+
+  restores.push({
+
+    date:
+    new Date().toLocaleString(),
+
+    status:
+    "Restauración completada"
+
+  });
+
+  localStorage.setItem(
+    "restores",
+    JSON.stringify(restores)
+  );
+
+  addAlert(
+    "Sistema restaurado correctamente"
+  );
+
+  alert(
+    "Datos restaurados correctamente."
+  );
+
+}

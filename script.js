47f093e92a59043173c20fdeb69c03e11c082159
ledo386 <davidoskipro08.2.0@gmail.com>
2026-05-28 09:59:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -108,55 +108,125 @@ document.getElementById("userArea");
 const loggedUser =
 localStorage.getItem("loggedUser");
 
-if(userArea && loggedUser){
+if(userArea){
 
-  let adminButton = "";
+  // =========================
+  // SIN LOGIN
+  // =========================
 
-  if(loggedUser === "admin"){
+  if(!loggedUser){
 
-    adminButton = `
-      <a href="admin.html">
-        Panel admin
+    userArea.innerHTML = `
+
+      <a href="./login.html"
+      class="login-button">
+
+        Iniciar sesión
+
       </a>
+
     `;
 
   }
 
-  userArea.innerHTML = `
+  // =========================
+  // CON LOGIN
+  // =========================
 
-    <div class="user-menu">
+  else{
 
-      <div class="user-profile">
+    let dashboardButton = "";
 
-        <span>👤</span>
+    // =========================
+    // ADMIN
+    // =========================
 
-        <span>${loggedUser}</span>
+    if(loggedUser === "admin"){
+
+      dashboardButton = `
+
+        <a href="./admin.html">
+
+          Panel Admin
+
+        </a>
+
+      `;
+
+    }
+
+    // =========================
+    // CLIENTE
+    // =========================
+
+    else{
+
+      dashboardButton = `
+
+        <a href="./client-dashboard.html">
+
+          Mi Panel
+
+        </a>
+
+      `;
+
+    }
+
+    // =========================
+    // MENÚ
+    // =========================
+
+    userArea.innerHTML = `
+
+      <div class="user-menu">
+
+        <div class="user-profile">
+
+          <span>👤</span>
+
+          <span>${loggedUser}</span>
+
+        </div>
+
+        <div class="user-dropdown">
+
+          ${dashboardButton}
+
+          <button onclick="logout()">
+
+            Cerrar sesión
+
+          </button>
+
+        </div>
 
       </div>
 
-      <div class="user-dropdown">
+    `;
 
-        ${adminButton}
-
-        <button onclick="logout()">
-          Cerrar sesión
-        </button>
-
-      </div>
-
-    </div>
-
-  `;
+  }
 
 }
 
+// =========================
+// LOGOUT
+// =========================
+
 function logout(){
 
-  localStorage.removeItem("loggedUser");
+  localStorage.removeItem(
+    "loggedUser"
+  );
+
+  localStorage.removeItem(
+    "managedUser"
+  );
 
   alert("Sesión cerrada.");
 
-  window.location.href = "index.html";
+  window.location.href =
+  "./index.html";
 
 }
 
